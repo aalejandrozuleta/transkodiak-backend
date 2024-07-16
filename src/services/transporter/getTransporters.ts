005ea52ff6a53f5575ecc831afd6f16588f4fa3a
Catalina Osorio <catalinaandrade026@gmail.com>
2024-07-16 10:35:58 -0500

    feature (getTransporters) Transporters

---
+++ src/services/transporter/getTransporters.ts
@@ -0,0 +1,7 @@
+//src/services/transporter/getTransporters.ts
+
+import { getAllTransporters } from '../../repositories/transporter/getTransporters';
+
+export const getTransporters = async () => {
+    return await getAllTransporters();
+};
