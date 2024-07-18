765065c59df77722defd6c236a8df9cc986a1ab9
Alejandro Zuleta <jhoitan266@gmail.com>
2024-07-17 19:20:56 -0500

    disable transporter

---
+++ src/repositories/transporter/disable.ts
@@ -0,0 +1,11 @@
+import { FieldPacket } from 'mysql2';
+import db from '@config/mysql';
+import { disableDto } from '@dto/transporter/disable';
+
+export  class disableRepository {
+  static async disableAccount(user:disableDto) {
+    const sql = 'CALL DisableStateTransporter(?)';
+    const values = [user.id];
+    return db.execute(sql, values);
+  }
+}
--- src/routes/transporterRoutes.ts
+++ src/routes/transporterRoutes.ts
@@ -11,5 +11,32 @@ import { registerController } from '@controller/transporter/register';
  * @access Público
  */
 
-routerTransporter.post('/register', registerValidator, registerController);
-// borrar
+routerTransporter.post('/register', registerValidator, registerController); 
+
+
+//* ----- GET TRANSPORTERS --------------------------------
+
+import { getTransportersController } from '@controller/transporter/getTransporters';
+
+/**
+ * @route GET /transporters
+ * @description Obtener todos los transportadores registrados
+ * @access Público
+ */
+
+routerTransporter.get('/listTransporters', getTransportersController);
+
+
+//* ----- DISABLE TRANSPORTER -----
+
+import { disableController } from '@controller/transporter/disable';
+import { disableValidator } from '@middleware/validation/transporter/disable';
+
+/**
+ * @route put /disable
+ * @description Deshabilitar un transportador
+ * @access Privado (JWT)
+ * 
+ */
+
+routerTransporter.put('/disable/:id', disableValidator, disableController);
