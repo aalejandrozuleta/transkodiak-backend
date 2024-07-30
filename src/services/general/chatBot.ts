aab03c883d98da645384232fc18cfe12a4f355dc
Alejandro Zuleta <jhoitan266@gmail.com>
2024-07-30 11:31:11 -0500

    limitation

--- src/services/general/chatBot.ts
+++ src/services/general/chatBot.ts
@@ -1,3 +1,5 @@
+// archivo: chatBotService.ts
+
 import { chatMessageInterface } from "@interfaces/general/chatMessageBot";
 import chatBotConfig from '@config/chatBot';
 import chatBotDto from '@dto/general/chatBot';
@@ -10,6 +12,51 @@ const API_KEY_GEMINI = process.env.GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(API_KEY_GEMINI as string);
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+// Temas permitidos y no permitidos
+const VALID_THEMES = [
+  "camiones",
+  "mercancías ",
+  "transporte ",
+  "logística",
+  "regulaciones",
+  "peajes",
+  "ciudades",
+  "Ganado",
+  'Misión',
+  'Vision',
+  'Descripción General',
+  'Fecha de Creación',
+  'Colombia',
+  'Carro',
+  'Combustible',
+  'Automóvil',
+  'Transporte público',
+  'Transporte privado',
+  'carga',
+  'peso',
+  'flete',
+  'promedio',
+  'conductores',
+  'conductore',
+  // Puedes añadir más temas permitidos aquí
+];
+
+const INVALID_THEMES = [
+  "religion",
+  "violencia",
+  "discriminación",
+  'Países'
+  // Puedes añadir más temas no permitidos aquí
+];
+
+const containsTheme = (text: string, themes: string[]): boolean => {
+  return themes.some(theme => text.toLowerCase().includes(theme.toLowerCase()));
+};
+
+const isValidResponse = (response: string): boolean => {
+  return containsTheme(response, VALID_THEMES) && !containsTheme(response, INVALID_THEMES);
+};
 
 export const chatBotService = async (chatBot: chatBotDto) => {
   // Transformar los datos de chatMessageInterface a Content
@@ -43,8 +90,11 @@ export const chatBotService = async (chatBot: chatBotDto) => {
     const response = sendQuestion.response;
     const text = response.text(); // Esperar el texto
 
-    // Solo devolver la respuesta del modelo y no todo el historial
-    return { response: text };
+    if (isValidResponse(text)) {
+      return { response: text };
+    } else {
+      throw new Error('La respuesta no está relacionada con los temas permitidos o contiene temas no permitidos.');
+    }
 
   } catch (error) {
     console.error('Error en el envío del mensaje:', error);
